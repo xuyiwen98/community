b3b5da1189145b9f22827996f23e47bb7907375e
doubleandyu <190848486@qq.com>
2020-03-12 18:57:29 +0800

    init repo

---
+++ src/main/java/com/xyw/community/contorller/indexController.java
@@ -0,0 +1,16 @@
+package com.xyw.community.contorller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class indexController {
+    @GetMapping("/")
+    public String index(){
+
+        return "index";
+    }
+}
