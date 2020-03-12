9ec2c75662085cc3ee0a2f71fb1a3ec4c6d42d69
doubleandyu <190848486@qq.com>
2020-03-12 19:28:36 +0800

    readme add url

--- README.md
+++ README.md
@@ -1,15 +1,10 @@
 ## 青柚社区
 
 ## 资料
-<<<<<<< HEAD
-https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/
-
-=======
 
 
 [BootStrap](https://v3.bootcss.com/getting-started/#download)
 [Github OAuth document](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
 
 
->>>>>>> readme add url
 ## 工具
---
+++ src/main/java/com/xyw/community/provider/GithubProvider.java
@@ -0,0 +1,50 @@
+package com.xyw.community.provider;
+
+import com.alibaba.fastjson.JSON;
+import com.xyw.community.dto.AccessTokenDTO;
+import com.xyw.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class GithubProvider {
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(mediaType,JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            //System.out.println(string);
+            //access_token=63e85f3632be4d39ecf8d2641fe494ffdcd144f0&scope=user&token_type=bearer
+            String token = string.split("&")[0].split("=")[1];
+            return token;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public GithubUser getUser(String accessToken){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token="+ accessToken)
+                .build();
+        try{
+            Response response = client.newCall(request).execute();
+            String string=response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        }catch (IOException e){
+
+        }
+        return null;
+
+    }
+}
